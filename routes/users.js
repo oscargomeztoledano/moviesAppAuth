a6d86ede2373b62f7ddf0a1abc2791b7ab6d8671
Iván González <ivan.gdiaz@uclm.es>
2022-11-08 14:47:30 +0100

    secure working well!

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,13 @@
 var express = require("express");
 var mongoose = require("mongoose");
 var jwt = require("jsonwebtoken");
-var crypto = require("crypto");
 var router = express.Router();
+
+// Token generation imports
+const dotenv = require('dotenv');
+// get config vars
+dotenv.config();
+
 var debug = require("debug")("moviesAppAuth:server");
 
 //Models
@@ -10,29 +15,46 @@ var User = require("../models/User.js");
 
 var db = mongoose.connection;
 
-//TOKEN_SECRET=crypto.randomBytes(64).toString('hex');
-TOKEN_SECRET="9f26e402586e2faa8da4c98a35f1b20d6b033c60";
-
-//Middleware utilizando "use"
-router.use("/secure", function (req, res, next) {
-    var retrievedToken = req.headers["authorization"]
+function tokenVerify (req, res, next) {
+    var authHeader=req.get('authorization');
+    const retrievedToken = authHeader.split(' ')[1];
+    
     if (!retrievedToken) {
         res.status(401).send({
             ok: false,
             message: "Token inválido"
         })
+    }else{       
+        jwt.verify(retrievedToken, process.env.TOKEN_SECRET, (err, retrievedToken) => {
+            if (err) {
+                debug(err.message);
+                res.status(401).send({
+                    ok: false,
+                    message: "Token inválido"
+                });
+            } else {
+                req.token = retrievedToken;
+                next();
+
+            }
+        });
     }
-    retrievedToken = retrievedToken.replace("Bearer ", "")
-    jwt.verify(retrievedToken, TOKEN_SECRET, function (err, retrievedToken) {
-        if (err) {
-            return res.status(401).send({
-                ok: false,
-                message: "Token inválido"
-            });
-        } else {
-            req.token = retrievedToken;
-            next();
-        }
+}
+
+router.get("/secure", tokenVerify, 
+function (req, res, next) {
+    debug("Acceso seguro con token a los usuarios");
+    User.find().sort("-creationdate").exec(function (err, users) {
+        if (err) res.status(500).send(err);
+        else res.status(200).json(users);
+    })
+});
+
+// GET del listado de usuarios ordenados por fecha de creación
+router.get("/", function (req, res, next) {
+    User.find().sort("-creationdate").exec(function (err, users) {
+        if (err) res.status(500).send(err);
+        else res.status(200).json(users);
     });
 });
 
@@ -52,8 +74,26 @@ router.get("/:id", function (req, res, next) {
     });
 });
 
+// GET de un único usuario por su Id
+router.get("/secure/:id", tokenVerify, function (req, res, next) {
+    debug("Acceso seguro con token a un usuario");
+    User.findById(req.params.id, function (err, userinfo) {
+        if (err) res.status(500).send(err);
+        else res.status(200).json(userinfo);
+    });
+});
+
 // POST de un nuevo usuario
 router.post("/", function (req, res, next) {
+    User.create(req.body, function (err, userinfo) {
+        if (err) res.status(500).send(err);
+        else res.sendStatus(200);
+    });
+});
+
+// POST de un nuevo usuario
+router.post("/secure", tokenVerify, function (req, res, next) {
+    debug("Creación de un usuario segura con token");
     User.create(req.body, function (err, userinfo) {
         if (err) res.status(500).send(err);
         else res.sendStatus(200);
@@ -68,8 +108,25 @@ router.put("/:id", function (req, res, next) {
     });
 });
 
+router.put("/secure/:id", tokenVerify, function (req, res, next) {
+    debug("Modificación segura de un usuario con token");
+    User.findByIdAndUpdate(req.params.id, req.body, function (err, userinfo) {
+        if (err) res.status(500).send(err);
+        else res.sendStatus(200);
+    });
+});
+
 // DELETE de un usuario existente identificado por su Id
 router.delete("/:id", function (req, res, next) {
+    User.findByIdAndDelete(req.params.id, function (err, userinfo) {
+        if (err) res.status(500).send(err);
+        else res.sendStatus(200);
+    });
+});
+
+// DELETE de un usuario existente identificado por su Id
+router.delete("/secure/:id", tokenVerify, function (req, res, next) {
+    debug("Borrado seguro de un usuario con token");
     User.findByIdAndDelete(req.params.id, function (err, userinfo) {
         if (err) res.status(500).send(err);
         else res.sendStatus(200);
@@ -136,15 +193,7 @@ function (req, res, next) {
 },
 function (req, res, next) {
     debug("... generando token");
-    // var u = {
-    //      username: req.body.userFound.username,
-    //      id: req.body.userFound.id
-    // };
-    var u = {
-        username: req.body.username,
-   };
-    jwt.sign(u, TOKEN_SECRET, {
-        expiresIn: 60 * 60 * 24 // expira en 24 horas...
+    jwt.sign({username: req.body.username},process.env.TOKEN_SECRET, {expiresIn: 3600 // expira en 24 horas...
     }, function(err, generatedToken) {
         if (err) res.status(500).send("¡Error generando token de autenticación");
         else res.status(200).send({
